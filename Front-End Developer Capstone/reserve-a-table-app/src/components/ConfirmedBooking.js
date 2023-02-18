0e686a4602240864e714ac5309920d637d245083
mwilkosz <micwilkosz@gmail.com>
2023-02-18 18:06:52 +0100

    Add initial ConfirmedBooking page

---
+++ Front-End Developer Capstone/reserve-a-table-app/src/components/ConfirmedBooking.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function ConfirmedBooking() {
+  return (
+    <div>
+      <h2>Booking Confirmed!</h2>
+      <p>Thank you for your reservation. We look forward to seeing you!</p>
+    </div>
+  );
+}
+
+export default ConfirmedBooking;
